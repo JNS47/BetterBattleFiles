900eca6ffe3d66ad810a379db374cf12a26c7fad
JNS47 <admin@jns47.de>
2020-06-17 20:34:49 +0200

    Changed %RUNDE_GEGNER% and %RUNDE_MC% to %GEGNER% and %HERAUSFORDERER% in default config

--- settings/config.js
+++ settings/config.js
@@ -7,6 +7,6 @@ const defaultConfig = {
   template_subtitle: "RBA Battle",
   template_title: "Battle vs. %RUNDE_GEGNER% %RUNDE_NAME%%RUNDE_NUMMER%",
   template_albumartist: "%RUNDE_MC% und %RUNDE_GEGNER%",
-  template_albumtitle: "RBA Battle %RUNDE_MC% vs. %RUNDE_GEGNER%",
+  template_albumtitle: "RBA Battle %HERAUSFORDERER% vs. %GEGNER%",
   template_filename: "RBA [%BATTLEID%] %HERAUSFORDERER% vs. %GEGNER% %RUNDE_NUMMER%%RUNDE_NAME%"
 };

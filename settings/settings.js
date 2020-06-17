289531385c9bf860a46373b5395c6d30e7caa1b5
JNS47 <admin@jns47.de>
2020-06-17 19:11:15 +0200

    moved script from inline to .js file (security policy)

--- settings/settings.js
+++ settings/settings.js
@@ -1,3 +1,59 @@
+//  set values of all inputs by their config value
+function setConfigValues(){
+  //  set default config if config is empty
+  getConfig((cfg) =>{
+    if(Object.keys(cfg).length < 1)
+      setConfigDefaults();
+    //  text / number inputs
+    const configOptionElements = document.querySelectorAll(".configOption");
+    for(let el of configOptionElements){
+      el.addEventListener("change", (event) =>{ setConfigOption(event.target.name, event.target.value); });
+      getConfigOption(el.name, (val) =>{
+        if(val)
+          el.value = val;
+      });
+    }
+
+    getConfigOption("extensionIsActive", (val) =>{
+      //  activated / deactivated button, + set class
+      const activeBtnElement = document.querySelector(".toggle-btn");
+      if(activeBtnElement){
+        activeBtnElement.addEventListener("click", (event) =>{ toggleExtension(event.target); });
+        if(val != activeBtnElement.classList.contains("active"))
+          toggleExtension(activeBtnElement);
+      }
+      const helpBtnElement = document.querySelector(".help-btn");
+      if(helpBtnElement)
+        helpBtnElement.addEventListener("click", () =>{ document.querySelector('.help').classList.toggle('hidden'); });
+      const resetBtnElement = document.querySelector(".click-btn");
+      if(resetBtnElement)
+        resetBtnElement.addEventListener("click", () =>{ resetConfig(); });
+    });
+  });
+}
+
+//  toggle class for the activated / deactivated button and save it to config
+function toggleExtension(el){
+  el.classList.toggle("active");
+  let text = "Aktivieren";
+  if(el.classList.contains("active"))
+    text = "Deaktivieren";
+  el.previousElementSibling.innerHTML = text;
+  setConfigOption("extensionIsActive", el.classList.contains("active"));
+}
+
+//  clear config and set it to it's defaults
+function resetConfig(){
+  clearConfig();
+  setConfigDefaults();
+  setConfigValues();
+}
+
+//  set config values on start
+setConfigValues();
+
+/*-----------------------------------------------------------------*/
+
 //  clear the config
 function clearConfig(){
   chrome.storage.sync.clear();
